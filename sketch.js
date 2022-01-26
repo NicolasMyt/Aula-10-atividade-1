7c37f3091ef45d420127583a6a0308df3f868c9b
NicolasMyt <98362140+NicolasMyt@users.noreply.github.com>
2022-01-26 19:19:11 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+
+function setup() 
+{
+  createCanvas(400,400);
+  var Pesos = [35,38,40,45];
+  var Soma = Pesos[0] + Pesos[1] + Pesos[2] + Pesos[3]
+  var Media = Soma/Pesos.length;
+  console.log(Media);
+}
+
+function draw() 
+{
+background(51);
+
+}
+
